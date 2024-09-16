10ef12dcb2ef99012849555b51271c5d58ddda69
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-09-16 14:39:15 -0400

    ass

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { EnterRomm } from '../game/game';
 import cors from "cors"
 import path from 'path';
+import { db } from './config/database';
 
 const app = express();
 const port = 3000;
@@ -15,8 +16,23 @@ app.set('views', path.resolve("src", "ui", "views"))
 // Configurar EJS como template engine
 app.set('view engine', 'ejs');
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
+  const user = await db.user.create({
+    data: {
+      email: "kjsdfads",
+      passwordHash: "asdfasdfasdpofasdfjp",
+      username: "gabriel"
+    }
+  })
+  
+  db.user.findUnique({
+    where: {
+      id: 1
+    }
+  })
   res.render("index.ejs");
+
+
 });
 
 app.get('/entrar/:name/:tableId', (req: Request, res: Response) => {
