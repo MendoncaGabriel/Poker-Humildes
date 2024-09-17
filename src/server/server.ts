29542d7a4d4b105571c931016b375fa3b98347a3
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-09-17 17:41:10 -0400

    adicionado websocket

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,21 +1,49 @@
-import express from 'express';
-import cors from "cors"
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 import path from 'path';
-import sitRouter from "./http/routers/auth/sitTable"
-import registerRouter from "./http/routers/auth/authRouter"
+import { WebSocketServer } from 'ws';
+import { insertPlayerInTable } from '../game/game';
 
 const app = express();
 const port = 3000;
 
-// configs
-app.use(cors())
-app.set('views', path.resolve("src", "ui", "views"))
+// Configurações
+app.use(cors());
+app.set('views', path.resolve('src', 'ui', 'views'));
 app.set('view engine', 'ejs');
 
-//api routers
-// app.use("/api/auth", registerRouter)
-app.use("/game", sitRouter)
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// WebSocket
+const server = app.listen(port, () => {
+  console.log(`Servidor HTTP rodando em http://localhost:${port}`);
 });
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Novo cliente conectado ao WebSocket.');
+
+  ws.on('message', async (data) => {
+    const message = JSON.parse(data.toString())
+    console.log(message);
+
+
+    // Envia a mensagem de volta para o cliente
+    ws.send(`Recebi sua mensagem!`);
+  });
+
+  ws.on('close', () => {
+    console.log('Cliente desconectado do WebSocket.');
+  });
+
+  ws.on('error', (error) => {
+    console.error('Erro no WebSocket: ', error);
+  });
+});
+
+console.log('WebSocket Server inicializado.');
+
+// Rotas
+app.get('/', (req: Request, res: Response) => {
+  res.render('index');
+});
+
